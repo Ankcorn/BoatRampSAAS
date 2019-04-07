80d7b42d7bbd395d6b2577e8c775719235a14f0f
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-04-07 14:36:10 +0100

    setup netlify functions and development proxy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './tailwind.css';
 
+async function makeApiCall() {
+  const data = await fetch('/.netlify/functions/file');
+  const json = await data.json()
+  console.log(json)
+}
 class App extends Component {
   render() {
     return (
       <div className="relative overflow-auto min-w-screen min-h-screen bg-gray-100">
-        <header className="flex shadow-2xl pl-3 items-center relative w-screen top-0 min-w-screen bg-gray-900">
+        <header className="flex shadow-2xl pl-3 items-center relative w-screen top-0 min-w-screen bg-gray-900" onClick={async () => await makeApiCall()}>
           <p className="pl-3 text-lg font-semibold text-gray-200">Boat Ramps AAS</p>
         </header>
       </div>
