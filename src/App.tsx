d2e985dd4e883a9e691708e8cd521a2482509ca8
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-04-07 17:09:50 +0100

    added fun icon

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="relative overflow-auto min-w-screen min-h-screen bg-gray-100">
         <header className="flex shadow-2xl pl-3 items-center relative w-screen top-0 min-w-screen bg-gray-900" onClick={async () => await makeApiCall()}>
-          <p className="pl-3 text-lg font-semibold text-gray-200">Boat Ramps AAS</p>
+          <Anchor color="#9f7aea" /><p className="pl-3 text-lg font-semibold text-gray-200">Boat Ramps AAS</p>
         </header>
         <Container>
           <Map />
